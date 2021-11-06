12c68465e8c46fe7738631e3b28bd8d8bdfdb0ff
Chris Calviello <chris.calviello@gmail.com>
2021-11-06 21:13:27 +0100

    add unit tests

---
+++ ApiUnitTesting/Models/AddressTest.cs
@@ -0,0 +1,24 @@
+﻿using Api.Models;
+using Xunit;
+
+namespace ApiUnitTesting.Models
+{
+    public class AddressTest
+    {
+        public AddressTest()
+        {
+        }
+
+        [Fact]
+        public void WhenCreate_ShouldSetValues()
+        {
+            var sut = new Address("IT", "CITY", "HOUSENUMBER", "STREET", "ZIPCODE");
+
+            Assert.Equal("IT", sut.Country);
+            Assert.Equal("CITY", sut.City);
+            Assert.Equal("HOUSENUMBER", sut.HouseNumber);
+            Assert.Equal("STREET", sut.Street);
+            Assert.Equal("ZIPCODE", sut.ZipCode);
+        }
+    }
+}
---
+++ ApiUnitTesting/Models/RegexAddressFormatTest.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using Api.Models;
+using Xunit;
+
+namespace ApiUnitTesting.Models
+{
+    public class RegexAddressFormatTest
+    {
+        public RegexAddressFormatTest()
+        {
+        }
+
+        [Fact]
+        public void WhenCreate_ShouldSetValues()
+        {
+            var sut = new RegexAddressFormat("NL", "/^([^0-9]*)$/", "^[0-9]", "/^([^0-9]*)$/", "/^(?:NL-)?(\\d{4})\\s*([A-Z]{2})$/i");
+
+            Assert.Equal("NL", sut.Country);
+            Assert.Equal("/^([^0-9]*)$/", sut.RegexCity);
+            Assert.Equal("^[0-9]", sut.RegexHouseNumber);
+            Assert.Equal("/^([^0-9]*)$/", sut.RegexStreet);
+            Assert.Equal("/^(?:NL-)?(\\d{4})\\s*([A-Z]{2})$/i", sut.RegexZipcode);
+        }
+
+        [Fact]
+        public void GivenInvalidRegexCityPattern_WhenCreate_ShouldSetValues()
+        {
+            Assert.Throws<Exception>(() => new RegexAddressFormat("NL", "[", "^[0-9]", "/^([^0-9]*)$/", "/^(?:NL-)?(\\d{4})\\s*([A-Z]{2})$/i"));
+        }
+
+        [Fact]
+        public void GivenInvalidRegexHouseNumberPattern_WhenCreate_ShouldSetValues()
+        {
+            Assert.Throws<Exception>(() => new RegexAddressFormat("NL", "/^([^0-9]*)$/", "[", "/^([^0-9]*)$/", "/^(?:NL-)?(\\d{4})\\s*([A-Z]{2})$/i"));
+        }
+
+        [Fact]
+        public void GivenInvalidRegexStreetPattern_WhenCreate_ShouldSetValues()
+        {
+            Assert.Throws<Exception>(() => new RegexAddressFormat("NL", "/^([^0-9]*)$/", "^[0-9]", "[", "/^(?:NL-)?(\\d{4})\\s*([A-Z]{2})$/i"));
+        }
+
+        [Fact]
+        public void GivenInvalidRegexZipCodePatternPattern_WhenCreate_ShouldSetValues()
+        {
+            Assert.Throws<Exception>(() => new RegexAddressFormat("NL", "/^([^0-9]*)$/", "^[0-9]", "/^([^0-9]*)$/", "["));
+        }
+
+
+    }
+}
