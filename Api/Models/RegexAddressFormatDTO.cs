38fad31cd2b479b719d8787e8c3b3789bf558726
Chris Calviello <chris.calviello@gmail.com>
2021-11-07 17:55:17 +0100

    add dto for RegexAddressFormat

---
+++ Api/Models/RegexAddressFormatDTO.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace Api.Models
+{
+    public class RegexAddressFormatDTO
+    {
+        public string Country { get; private set; }
+        public bool IsCityOptional { get; private set; }
+        public bool IsHouseNumberOptional { get; private set; }
+        public bool IsStreetOptional { get; private set; }
+        public bool IsZipcodeOptional { get; private set; }
+
+        public RegexAddressFormatDTO(RegexAddressFormat regexAddressFormat)
+        {
+            Country = regexAddressFormat.Country;
+
+            IsCityOptional = IsFieldOptional(regexAddressFormat.RegexCity);
+            IsHouseNumberOptional = IsFieldOptional(regexAddressFormat.RegexHouseNumber);
+            IsStreetOptional = IsFieldOptional(regexAddressFormat.RegexStreet);
+            IsZipcodeOptional = IsFieldOptional(regexAddressFormat.RegexZipcode);
+        }
+
+        private static bool IsFieldOptional(string pattern)
+        {
+            if (string.IsNullOrWhiteSpace(pattern)) return true;
+
+            try
+            {
+                return Regex.IsMatch("", pattern);
+            }
+            catch (Exception)
+            {
+                return true;
+            }
+        }
+    }
+}
---
+++ ApiUnitTesting/Models/RegexAddressFormatDTOTest.cs
@@ -0,0 +1,36 @@
+﻿using Api.Models;
+using Xunit;
+
+namespace ApiUnitTesting.Models
+{
+    public class RegexAddressFormatDTOTest
+    {
+        private readonly RegexAddressFormat regexAddressFormatWithAllRequiredFields;
+        private readonly RegexAddressFormat regexAddressFormatWithAllOptionalFields;
+
+        public RegexAddressFormatDTOTest()
+        {
+            regexAddressFormatWithAllRequiredFields = new RegexAddressFormat("NL", "[\\p{L} ]+$", "^[0-9]", "[\\p{L} ]+$", "^([0-9]{3})$");
+            regexAddressFormatWithAllOptionalFields = new RegexAddressFormat("NL", "[\\p{L} ]?$", "^[0-9]?", "[\\p{L} ]?$", "^([0-9]{3})?$");
+        }
+
+        [Fact]
+        public void WhenCreate_ShouldSetFields()
+        {
+            var sut = new RegexAddressFormatDTO(regexAddressFormatWithAllRequiredFields);
+
+            Assert.Equal(sut.Country, regexAddressFormatWithAllRequiredFields.Country);
+            Assert.False(sut.IsCityOptional);
+            Assert.False(sut.IsHouseNumberOptional);
+            Assert.False(sut.IsStreetOptional);
+            Assert.False(sut.IsZipcodeOptional);
+
+            sut = new RegexAddressFormatDTO(regexAddressFormatWithAllOptionalFields);
+            Assert.Equal(sut.Country, regexAddressFormatWithAllOptionalFields.Country);
+            Assert.True(sut.IsCityOptional);
+            Assert.True(sut.IsHouseNumberOptional);
+            Assert.True(sut.IsStreetOptional);
+            Assert.True(sut.IsZipcodeOptional);
+        }
+    }
+}
