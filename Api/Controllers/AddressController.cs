531a8777276751a3764760b084657934201b0ffd
Chris Calviello <chris.calviello@gmail.com>
2021-11-07 16:19:42 +0100

    add api/address/isvalid endpoint

---
+++ Api/Controllers/AddressController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using Api.Models;
+using Api.Services.AddressValidation;
+using Api.Services.Config;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Api.Controllers
+{
+    [Route("api/address")]
+    public class AddressController : ControllerBase
+    {
+        private readonly IConfigService configService;
+        private readonly IAddressValidationService addressValidationService;
+
+        public AddressController()
+        {
+            configService = new JsonConfigService("config/AddressesFormat.json");
+            addressValidationService = new AddressValidationService(configService);
+        }
+
+        [HttpPost]
+        [Route("isvalid")]
+        public IActionResult IsValid([FromBody] Address address)
+        {
+            try
+            {
+                return Ok(new { IsValid = addressValidationService.IsValid(address) });
+            }
+            catch(Exception ex)
+            {
+                return StatusCode(500, new { Error = ex.Message});
+            }
+        }
+    }
+}
