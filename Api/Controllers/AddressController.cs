66c026e707883ff211eb2618f26d57395c2b0a59
Chris Calviello <chris.calviello@gmail.com>
2021-11-07 17:21:49 +0100

    add delete, edit, add endpoints

--- Api/Controllers/AddressController.cs
+++ Api/Controllers/AddressController.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using Api.Models;
 using Api.Services.AddressValidation;
+using Api.Services.Config;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Api.Controllers
@@ -9,10 +10,12 @@ namespace Api.Controllers
     public class AddressController : ControllerBase
     {
         private readonly IAddressValidationService addressValidationService;
+        private readonly IConfigService configService;
 
-        public AddressController(IAddressValidationService addressValidationService)
+        public AddressController(IAddressValidationService addressValidationService, IConfigService configService)
         {
             this.addressValidationService = addressValidationService;
+            this.configService = configService;
         }
 
         [HttpPost]
@@ -26,6 +29,51 @@ namespace Api.Controllers
             catch(Exception ex)
             {
                 return StatusCode(500, new { Error = ex.Message});
+            }
+        }
+
+        [HttpPost]
+        [Route("")]
+        public IActionResult AddAddressesFormat([FromBody] RegexAddressFormat regexAddressFormat)
+        {
+            try
+            {
+                configService.AddRegexAddressFormat(regexAddressFormat);   
+                return Ok();
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, new { Error = ex.Message });
+            }
+        }
+
+        [HttpPut]
+        [Route("")]
+        public IActionResult EditAddressesFormat([FromBody] RegexAddressFormat regexAddressFormat)
+        {
+            try
+            {
+                configService.EditRegexAddressFormat(regexAddressFormat);
+                return Ok();
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, new { Error = ex.Message });
+            }
+        }
+
+        [HttpDelete]
+        [Route("")]
+        public IActionResult Delete([FromQuery] string countryCode)
+        {
+            try
+            {
+                configService.DeleteRegexAddressFormat(countryCode);
+                return Ok();
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, new { Error = ex.Message });
             }
         }
     }
