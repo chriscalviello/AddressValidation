993ef6d7f842380c0dffcd97f2bd7a42d2a79c91
Chris Calviello <chris.calviello@gmail.com>
2021-11-08 16:57:00 +0100

    add auth routes

---
+++ Api/Controllers/AuthenticationController.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using Api.Models.User;
+using Api.Services.Authentication;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Api.Controllers
+{
+    [ApiController]
+    [Route("api/auth")]
+    public class AuthenticationController : ControllerBase
+    {
+        private readonly IAuthenticationService authenticationService;
+
+        public AuthenticationController(IAuthenticationService authenticationService)
+        {
+            this.authenticationService = authenticationService;
+        }
+
+        /// <summary>
+        /// Add new credentials and get a jwt token
+        /// </summary>
+        [HttpPost]
+        [Route("signup")]
+        [ProducesResponseType(200, Type = typeof(LoggedUser))]
+        public IActionResult Signup([FromBody] Credentials credentials)
+        {
+            try
+            {
+                return Ok(new { IsValid = authenticationService.Signup(credentials) });
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, new { Error = ex.Message });
+            }
+        }
+
+        /// <summary>
+        /// Login and get a jwt token
+        /// </summary>
+        [HttpPost]
+        [Route("signin")]
+        [ProducesResponseType(200, Type = typeof(LoggedUser))]
+        public IActionResult Signin([FromBody] Credentials credentials)
+        {
+            try
+            {
+                return Ok(new { IsValid = authenticationService.Signin(credentials) });
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, new { Error = ex.Message });
+            }
+        }
+    }
+}
